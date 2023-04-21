56b9978995dbd90d5572b02672988797eea307c3
Ahmedkr21 <ahmedkamalr2001@gmail.com>
2023-04-22 02:27:44 +0400

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,8 @@ import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
+import '../styles/globals.css'
+
 setupIonicReact()
 
 
